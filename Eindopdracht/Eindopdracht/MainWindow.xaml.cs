4e18d4f9ce27ec88895a662b3d5733e9f44e6b47
RonaldElzen <ronald_elzen@hotmail.com>
2016-03-31 14:41:06 +0200

    Deleted classes and imported dll

--- Eindopdracht/Eindopdracht/MainWindow.xaml.cs
+++ Eindopdracht/Eindopdracht/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ad;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace Eindopdracht
 {
