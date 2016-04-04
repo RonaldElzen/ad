e4c10307bf3b7576999170d8e6dd6a1ba4b011f5
RonaldElzen <ronald_elzen@hotmail.com>
2016-04-04 18:42:21 +0200

    test methods bucket hash

--- ad/ad/BucketHash.cs
+++ ad/ad/BucketHash.cs
@@ -9,66 +9,111 @@ namespace ad
     using System.Threading.Tasks;
 
 
-        public class bucketHash
+    public class bucketHash
+    {
+        //Needs to be dynamic
+        int size = 0;
+        ArrayList[] data;
+        public bucketHash(int size)
         {
-            //Needs to be dynamic
-            int size = 0;
-            ArrayList[] data;
-            public bucketHash(int size)
+            data = new ArrayList[size];
+            for (int i = 0; i < size - 1; i++)
             {
-                data = new ArrayList[size];
-                for (int i = 0; i < size - 1; i++)
-                {
-                    data[i] = new ArrayList(4);
-                }
+                data[i] = new ArrayList(4);
             }
-            public int Hash<T>(T item) where T : IComparable<T>
-            {
-                long tot = 0;
-                char[] charray;
-                
-                //Make item a string and make a chararray
-                //NEEDS TO BE CHECKED
+        }
 
-                charray = item.ToString().ToCharArray();
-                for (int i = 0; i < item.ToString().Length - 1; i++)
-                {
-                    tot += 37 * tot + (int)charray[i];
-                }
-                tot = tot % data.GetUpperBound(0);
-                if (tot < 0)
-                {
-                    tot += data.GetUpperBound(0);
-                }
-                return (int)tot;
+
+
+        public int Hash(string s)
+        {
+            long tot = 0;
+            char[] charray;
+            charray = s.ToCharArray();
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                tot += 37 * tot + (int)charray[i];
             }
-
-
-            public void Insert<T>(T item) where T : IComparable<T>
+            tot = tot % data.GetUpperBound(0);
+            if (tot < 0)
             {
-                int hash_value;
-                hash_value = Hash(item);
-                if (data[hash_value].Contains(item))
-                {
-                    data[hash_value].Add(item);
-                }
+                tot += data.GetUpperBound(0);
             }
+            return (int)tot;
+        }
 
-            public void Remove<T>(T item) where T : IComparable<T>
+
+
+
+        //public int Hash<T>(T item) where T : IComparable<T>
+        //{
+        //long tot = 0;
+        //char[] charray;
+
+        ////Make item a string and make a chararray
+        ////NEEDS TO BE CHECKED
+
+        //charray = item.ToString().ToCharArray();
+        //for (int i = 0; i < item.ToString().Length - 1; i++)
+        //{
+        //    tot += 37 * tot + (int)charray[i];
+        //}
+        //tot = tot % data.GetUpperBound(0);
+        //if (tot < 0)
+        //{
+        //    tot += data.GetUpperBound(0);
+        //}
+        //return (int)tot;
+
+
+
+        //public void Insert<T>(T item) where T : IComparable<T>
+        //    {
+        //        int hash_value;
+        //        hash_value = Hash(item);
+        //        if (data[hash_value].Contains(item))
+        //        {
+        //            data[hash_value].Add(item);
+        //        }
+        //    }
+
+        //    public void Remove<T>(T item) where T : IComparable<T>
+        //    {
+        //        int hash_value;
+        //        hash_value = Hash(item);
+        //        if (data[hash_value].Contains(item))
+        //        {
+        //            data[hash_value].Remove(item);
+        //        }
+        //    }
+
+        //    public ArrayList[] getBucketHash()
+        //    {
+        //        return data;
+        //    }
+        //}
+        public void Insert(string item)
+        {
+            int hash_value;
+            hash_value = Hash(item);
+            if (data[hash_value].Contains(item))
             {
-                int hash_value;
-                hash_value = Hash(item);
-                if (data[hash_value].Contains(item))
-                {
-                    data[hash_value].Remove(item);
-                }
+                data[hash_value].Add(item);
             }
+        }
 
-            public ArrayList[] getBucketHash()
+        public void Remove(string item)
+        {
+            int hash_value;
+            hash_value = Hash(item);
+            if (data[hash_value].Contains(item))
             {
-                return data;
+                data[hash_value].Remove(item);
             }
         }
     }
+}
+
+    
 
 
