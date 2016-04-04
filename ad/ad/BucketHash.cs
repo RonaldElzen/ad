285f2acb6148933fa78878ee0378bbbaaff31b46
RonaldElzen <ronald_elzen@hotmail.com>
2016-04-04 19:17:07 +0200

    Fixed method insert

--- ad/ad/BucketHash.cs
+++ ad/ad/BucketHash.cs
@@ -9,13 +9,15 @@ namespace ad
     using System.Threading.Tasks;
 
 
-    public class bucketHash
+    public class bucketHash<T>
     {
-        //Needs to be dynamic
+       
         int size = 0;
         ArrayList[] data;
         public bucketHash(int size)
         {
+
+
             data = new ArrayList[size];
             for (int i = 0; i < size - 1; i++)
             {
@@ -35,6 +37,7 @@ namespace ad
                 tot += 37 * tot + (int)charray[i];
             }
             tot = tot % data.GetUpperBound(0);
+
             if (tot < 0)
             {
                 tot += data.GetUpperBound(0);
@@ -45,64 +48,40 @@ namespace ad
 
 
 
-        //public int Hash<T>(T item) where T : IComparable<T>
-        //{
-        //long tot = 0;
-        //char[] charray;
+        public int Hash<T>(T item)
+        {
+            long tot = 0;
+            char[] charray;
 
-        ////Make item a string and make a chararray
-        ////NEEDS TO BE CHECKED
+            //Make item a string and make a chararray
+            //NEEDS TO BE CHECKED
 
-        //charray = item.ToString().ToCharArray();
-        //for (int i = 0; i < item.ToString().Length - 1; i++)
-        //{
-        //    tot += 37 * tot + (int)charray[i];
-        //}
-        //tot = tot % data.GetUpperBound(0);
-        //if (tot < 0)
-        //{
-        //    tot += data.GetUpperBound(0);
-        //}
-        //return (int)tot;
+            charray = item.ToString().ToCharArray();
+            for (int i = 0; i < item.ToString().Length - 1; i++)
+            {
+                tot += 37 * tot + (int)charray[i];
+            }
+            tot = tot % data.GetUpperBound(0);
+            if (tot < 0)
+            {
+                tot += data.GetUpperBound(0);
+            }
+            return (int)tot;
+        }
 
 
 
-        //public void Insert<T>(T item) where T : IComparable<T>
-        //    {
-        //        int hash_value;
-        //        hash_value = Hash(item);
-        //        if (data[hash_value].Contains(item))
-        //        {
-        //            data[hash_value].Add(item);
-        //        }
-        //    }
-
-        //    public void Remove<T>(T item) where T : IComparable<T>
-        //    {
-        //        int hash_value;
-        //        hash_value = Hash(item);
-        //        if (data[hash_value].Contains(item))
-        //        {
-        //            data[hash_value].Remove(item);
-        //        }
-        //    }
-
-        //    public ArrayList[] getBucketHash()
-        //    {
-        //        return data;
-        //    }
-        //}
-        public void Insert(string item)
+        public void Insert<T>(T item)
         {
             int hash_value;
             hash_value = Hash(item);
-            if (data[hash_value].Contains(item))
+            if (!data[hash_value].Contains(item))
             {
                 data[hash_value].Add(item);
             }
         }
 
-        public void Remove(string item)
+        public void Remove<T>(T item)
         {
             int hash_value;
             hash_value = Hash(item);
@@ -111,6 +90,12 @@ namespace ad
                 data[hash_value].Remove(item);
             }
         }
+
+        public ArrayList[] getBucketHash()
+            {
+                return data;
+            }
+      
     }
 }
 
