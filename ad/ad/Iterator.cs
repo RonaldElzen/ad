6d96f04dadbd9679ea6a3eac4749fd616e1cecef
wouterbrink <wouterr.brink@hotmail.com>
2016-04-05 20:01:36 +0200

    Iterator done
    is klaar

--- ad/ad/Iterator.cs
+++ ad/ad/Iterator.cs
@@ -12,24 +12,35 @@ namespace ad
         private LNode<T> current;
         private LNode<T> prev;
 
-        public Iterator(LinkedList<T> list)
+        public Iterator(LinkedList<T> newList)
         {
-            this.list = list;
+            list = newList;
             current = list.getHeader();
             prev = null;
         }
 
+        /// <summary>
+        /// go to the next node in the list
+        /// </summary>
         public void next()
         {
             prev = current;
             current = current.next;
         }
 
+        /// <summary>
+        /// returns which node is the current node
+        /// </summary>
+        /// <returns>current node</returns>
         public LNode<T> getCurrent()
         {
             return current;
         }
 
+        /// <summary>
+        /// inserts the new item before the current node
+        /// </summary>
+        /// <param name="newItem"></param>
         public void insertBefore(T newItem)
         {
             LNode<T> item = new LNode<T>(newItem);
@@ -38,27 +49,41 @@ namespace ad
             current = item;
         }
 
-        public void insterAfer(T newItem)
+        /// <summary>
+        /// inserts the new item after the current node
+        /// </summary>
+        /// <param name="newItem"></param>
+        public void insterAfter(T newItem)
         {
             LNode<T> item = new LNode<T>(newItem);
             item.next = current.next;
             current.next = item;
         }
 
+        /// <summary>
+        /// removes the current node
+        /// </summary>
         public void remove()
         {
             prev.next = current.next;
         }
 
+        /// <summary>
+        /// clears the list
+        /// </summary>
         public void clear()
         {
             current = list.getHeader();
             prev = null;
         }
 
+        /// <summary>
+        /// check if the iterator has reached the end
+        /// </summary>
+        /// <returns></returns>
         public bool end()
         {
-            return (current.next == null);
+            return (current == null);
         }
     }
 }
